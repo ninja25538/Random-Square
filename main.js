0ba70a7d60dd078a2c58ec786fb1f94ebc228d8b
ninja25538 <ninja25538@users.noreply.github.com>
2017-05-11 16:31:05 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
      ctx.canvas.width = window.innerWidth - 55;
      ctx.canvas.height = window.innerHeight - 150;
-var crazoX = ctx.canvas.width/2;
-var crazoY = ctx.canvas.height/2;
+var squareX = ctx.canvas.width/2;
+var squareY = ctx.canvas.height/2;
 
 var epilepsyWarning = document.getElementById("Epilepsy");
 epilepsyWarning.innerHTML = "Warning: Flashing Colors in 3 seconds";
@@ -16,36 +16,27 @@ setInterval(function(){
   var hue = 'rgb(' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ')';
   var random = Math.floor((Math.random() * 4) + 1);
   ctx.fillStyle = hue;
-  ctx.fillRect(crazoX, crazoY, 15, 15);
-  if(random === 1 && crazoX <= ctx.canvas.width){
+  ctx.fillRect(squareX, squareY, 15, 15);
+  if(random === 1 && squareX <= ctx.canvas.width){
     hue = 'rgb(' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ')';
   ctx.fillStyle = hue;
-    crazoX+= 5;
-    if(crazoX === player1X && crazoY === player1Y || crazoX === player2X && crazoY === player2Y){
-      alert("You died!");
-    }
-    ctx.fillRect(crazoX, crazoY, 15, 15);
-  } else if(random === 2 && crazoY <= ctx.canvas.height){
+    squareX+= 5;
+    ctx.fillRect(squareX, squareY, 15, 15);
+  } else if(random === 2 && squareY <= ctx.canvas.height){
     hue = 'rgb(' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ')';
   ctx.fillStyle = hue;
-    crazoY+= 5;
-    ctx.fillRect(crazoX, crazoY, 15, 15);
-        if(crazoX === player1X && crazoY === player1Y || crazoX === player2X && crazoY === player2Y){
-      alert("You died!");
-    }
-  } else if(random === 3 && crazoX >= 0){
+    squareY+= 5;
+    ctx.fillRect(squareX, squareY, 15, 15);
+  } else if(random === 3 && squareX >= 0){
     hue = 'rgb(' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ')';
       ctx.fillStyle = hue;
-    crazoX-= 5;
-    ctx.fillRect(crazoX, crazoY, 15, 15);
-        if(crazoX === player1X && crazoY === player1Y || crazoX === player2X && crazoY === player2Y){
-      alert("You died!");
-    }
-  } else if(random === 4 && crazoY >= 0){
+    squareX-= 5;
+    ctx.fillRect(squareX, squareY, 15, 15);
+  } else if(random === 4 && squareY >= 0){
     hue = 'rgb(' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ')';
   ctx.fillStyle = hue;
-    crazoY-= 5;
-    ctx.fillRect(crazoX, crazoY, 15, 15);
+    squareY-= 5;
+    ctx.fillRect(squareX, squareY, 15, 15);
   }
 }, 10);
 }, 3000);
