a43beb1f5d1decedf73389a7f6284ebce4eb78a1
ninja25538 <ninja25538@users.noreply.github.com>
2017-05-11 16:33:13 -0400

    Update main.js

--- main.js
+++ main.js
@@ -16,27 +16,27 @@ setInterval(function(){
   var hue = 'rgb(' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ')';
   var random = Math.floor((Math.random() * 4) + 1);
   ctx.fillStyle = hue;
-  ctx.fillRect(squareX, squareY, 1, 1);
+  ctx.fillRect(squareX, squareY, 5, 5);
   if(random === 1 && squareX <= ctx.canvas.width){
     hue = 'rgb(' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ')';
   ctx.fillStyle = hue;
     squareX+= 5;
-    ctx.fillRect(squareX, squareY, 1, 1);
+    ctx.fillRect(squareX, squareY, 5, 5);
   } else if(random === 2 && squareY <= ctx.canvas.height){
     hue = 'rgb(' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ')';
   ctx.fillStyle = hue;
     squareY+= 5;
-    ctx.fillRect(squareX, squareY, 1, 1);
+    ctx.fillRect(squareX, squareY, 5, 5);
   } else if(random === 3 && squareX >= 0){
     hue = 'rgb(' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ')';
       ctx.fillStyle = hue;
     squareX-= 5;
-    ctx.fillRect(squareX, squareY, 1, 1);
+    ctx.fillRect(squareX, squareY, 5, 5);
   } else if(random === 4 && squareY >= 0){
     hue = 'rgb(' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ')';
   ctx.fillStyle = hue;
     squareY-= 5;
-    ctx.fillRect(squareX, squareY, 1, 1);
+    ctx.fillRect(squareX, squareY, 5, 5);
   }
 }, 10);
 }, 3000);
