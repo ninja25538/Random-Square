d5bb4fcacf75819c4d76fba52fce1d922d31b1d5
ninja25538 <ninja25538@users.noreply.github.com>
2017-08-24 14:22:23 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var ctx = c.getContext("2d");
      ctx.canvas.height = window.innerHeight - 150;
 var squareX = ctx.canvas.width/2;
 var squareY = ctx.canvas.height/2;
-var size = prompt("Pixel Size Of Squares?");
+var size = 5
 
 var epilepsyWarning = document.getElementById("Epilepsy");
 epilepsyWarning.innerHTML = "Warning: Flashing Colors in 3 seconds";
